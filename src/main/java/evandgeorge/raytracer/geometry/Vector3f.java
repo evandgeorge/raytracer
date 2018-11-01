e5921dd951ed4ca7a7ade5af1d0625e180a8f27d
George <19EGeorge@stu.hiltoncsd.net>
2018-11-01 14:11:19 -0400

    Added: add(Vector3f), subtract(Vector3f), multiply(double), divide(double), dotProduct(Vector3f)
    and respective tests, also made standard assertEquals delta (1e-10)

--- src/main/java/evandgeorge/raytracer/geometry/Vector3f.java
+++ src/main/java/evandgeorge/raytracer/geometry/Vector3f.java
@@ -22,6 +22,46 @@ public class Vector3f {
     public Vector3f normalize() {                   //returns a vector with the same direction and magnitude of 1
         double magnitude = magnitude();
 
-        return new Vector3f(x / magnitude, y / magnitude, z / magnitude);
+        return this.divide(magnitude);
+    }
+
+    public Vector3f add(Vector3f v3f) {             //returns sum of vectors
+        double sx = this.x + v3f.x;
+        double sy = this.y + v3f.y;
+        double sz = this.z + v3f.z;
+
+        return new Vector3f(sx, sy, sz);
+    }
+
+    public Vector3f subtract(Vector3f v3f) {        //returns difference between vectors
+        double dx = this.x - v3f.x;
+        double dy = this.y - v3f.y;
+        double dz = this.z - v3f.z;
+
+        return new Vector3f(dx, dy, dz);
+    }
+
+    public Vector3f multiply(double f) {            //returns vector after its multiplied by f
+        double px = this.x * f;
+        double py = this.y * f;
+        double pz = this.z * f;
+
+        return new Vector3f(px, py, pz);
+    }
+
+    public Vector3f divide(double d) {              //returns vector after its divided by d
+        double qx = this.x / d;
+        double qy = this.y / d;
+        double qz = this.z / d;
+
+        return new Vector3f(qx, qy, qz);
+    }
+
+    public double dotProduct(Vector3f v3f) {        //returns dot product of vector
+        double xProd = this.x * v3f.x;
+        double yProd = this.y * v3f.y;
+        double zProd = this.z * v3f.z;
+
+        return xProd + yProd + zProd;
     }
 }
--- src/test/java/evandgeorge/raytracer/geometry/Vector3f_test.java
+++ src/test/java/evandgeorge/raytracer/geometry/Vector3f_test.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 
 public class Vector3f_test {
 
+    private final double DELTA = 1e-10;
+
     @Test
     public void test_storesXYZ() {
         double x = .234;
@@ -26,7 +28,7 @@ public class Vector3f_test {
         
         Vector3f v3f = new Vector3f(x, y, z);
         
-        assertEquals(Math.sqrt(50), v3f.magnitude(), .001);
+        assertEquals(Math.sqrt(50), v3f.magnitude(), DELTA);
     }
 
     @Test
@@ -50,6 +52,60 @@ public class Vector3f_test {
         
         Vector3f v3f = new Vector3f(x, y, z);
 
-        assertEquals(1, v3f.normalize().magnitude(), .001);
+        assertEquals(1, v3f.normalize().magnitude(), DELTA);
     }
+
+    @Test
+    public void test_add() {
+        Vector3f v1 = new Vector3f(5.2, 4.2, 6);
+        Vector3f v2 = new Vector3f(7, 2.2, -3);
+
+        Vector3f vSum = v1.add(v2);
+        assertEquals(12.2, vSum.x, DELTA);
+        assertEquals(6.4, vSum.y, DELTA);
+        assertEquals(3, vSum.z, DELTA);
+    }
+
+    @Test
+    public void test_subtract() {
+        Vector3f v1 = new Vector3f(5.2, 4.2, 6);
+        Vector3f v2 = new Vector3f(7, 2.2, -3);
+
+        Vector3f vDif = v1.subtract(v2);
+        assertEquals(-1.8, vDif.x, DELTA);
+        assertEquals(2, vDif.y, DELTA);
+        assertEquals(9, vDif.z, DELTA);
+    }
+
+    @Test
+    public void test_multiply() {
+        Vector3f v1 = new Vector3f(-5, 6.6, 6);
+
+        Vector3f vProduct = v1.multiply(1.5);
+
+        assertEquals(-7.5, vProduct.x, DELTA);
+        assertEquals(9.9, vProduct.y, DELTA);
+        assertEquals(9, vProduct.z, DELTA);
+    }
+
+    @Test
+    public void test_divide() {
+        Vector3f v1 = new Vector3f(-7, 3.2, .25);
+
+        Vector3f vQuotient = v1.divide(2.5);
+
+        assertEquals(-2.8, vQuotient.x, DELTA);
+        assertEquals(1.28, vQuotient.y, DELTA);
+        assertEquals(.1, vQuotient.z, DELTA);
+    }
+
+    @Test
+    public void test_dotProduct() {
+        Vector3f v1 = new Vector3f(-5, 6, 6);
+        Vector3f v2 = new Vector3f(7, 2.5, -3);
+
+        assertEquals(-38, v1.dotProduct(v2), DELTA);
+        assertEquals(-38, v2.dotProduct(v1), DELTA);
+    }
+
 }
