6fa7796d47d1d19d26fee508497a72f48420cea4
Evan George <evandgeorge@gmail.com>
2018-11-03 13:32:24 -0400

    Refactored Ray

--- src/main/java/evandgeorge/raytracer/geometry/Ray.java
+++ src/main/java/evandgeorge/raytracer/geometry/Ray.java
@@ -2,12 +2,11 @@ package evandgeorge.raytracer.geometry;
 
 public class Ray {
 
-    Vector3f pos, dir;
+    Vector3f posV, dirV;  //position and direction vectors of ray
 
-    public Ray(Vector3f pos, Vector3f dir) {
-        this.pos = pos;
-        this.dir = dir;
+    public Ray(Vector3f posV, Vector3f dirV) {
+        this.posV = posV;
+        this.dirV = dirV;
     }
 
-    
 }
